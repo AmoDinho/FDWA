729b5f697109ae08c8ed47b752e4ab78b51898fb
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-04 07:37:33 +0200

    ADD => 404 Routing

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,9 +2,12 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import helloWorld from "./containers/helloWorld";
+import NotFound from "./containers/NotFound";
 
 export default () =>
   <Switch>
     <Route exact path="/" component={ Home } />
     <Route exact path="/queries/helloWorld" component={ helloWorld } />
+    { /* Error 404 */ }
+    <Route component={NotFound} />
   </Switch>;
